818697b4b37056eec4f354e58d9ed616577d69a0
Arjun Nourmansyah Ramdani <arjunnourmansyah27@gmail.com>
2025-03-30 01:44:07 +0700

    add import Global.css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFrown } from '@fortawesome/free-solid-svg-icons';
-import './App.css';
+import './Global.css';
 import Clock from './Clock';
 import LocationWeather from './LocationWeather';
 
